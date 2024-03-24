08b5bf5f3d2a2841f9fc87a0c118b0e6bec83ae5
Bykhkalo <codit.bykhkalo@gmail.com>
2024-03-24 15:05:53 +0100

    remove unused method

--- app/src/main/java/com/birthdayapp/sample/data/model/Profile.kt
+++ app/src/main/java/com/birthdayapp/sample/data/model/Profile.kt
@@ -9,11 +9,5 @@ data class Profile(
     companion object {
 
         const val DEFAULT_BIRTH_DATE = -1L
-
-        fun empty(): Profile = Profile(
-            name = "",
-            birthDate = DEFAULT_BIRTH_DATE,
-            photoPath = "",
-        )
     }
 }
